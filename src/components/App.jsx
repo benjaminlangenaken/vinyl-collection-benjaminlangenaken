d768aa68cbeae7eeec37b20d4ae9d03d2dcac663
benjaminlangenaken <benjamin.langenaken@outlook.com>
2022-04-20 14:37:06 +0200

    Create homepage (basic)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import '../App.css';
+import AlbumList from './AlbumList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [albums, setAlbums] = useState([
+		{
+			id: 1,
+			title: 'Kind of Blue',
+			artist: 'Miles Davis',
+			releaseYear: 1959,
+			genre: 'Jazz',
+			imgUrl: 'https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-031-Miles-Davis-KIND-OF-BLUE.jpg?w=1000',
+		},
+		{
+			id: 2,
+			title: 'Blue',
+			artist: 'Joni Mitchell',
+			releaseYear: 1971,
+			genre: 'Folk',
+			imgUrl: 'https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-003-JoniMitchell-BLUE-HR.jpg?w=1000',
+		},
+	]);
+
+	return (
+		<div className="vinyl-app-container">
+			<div className="add-album-container">
+				{albums.length === 0 ? (
+					<>
+						<h2>Start adding some vinyl!</h2>
+						<form action="#">
+							<input
+								type="text"
+								className="vinyl-title-input"
+								placeholder="Please enter the album title..."
+								defaultValue=""
+							/>
+						</form>
+					</>
+				) : (
+					<h1>My collection</h1>
+				)}
+			</div>
+
+			<AlbumList albums={albums} />
+		</div>
+	);
 }
 
 export default App;
