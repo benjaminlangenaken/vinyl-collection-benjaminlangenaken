0c82d2415d4ca55df30cc8befa1f4a5eb6984702
benjaminlangenaken <benjamin.langenaken@outlook.com>
2022-04-21 14:09:09 +0200

    Add AlbumForm Component

---
+++ src/components/AlbumForm.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+
+const AlbumForm = (props) => {
+	const [albumId, setAlbumId] = useState(3);
+	const [albumTitle, setAlbumTitle] = useState('');
+	const [albumArtist, setAlbumArtist] = useState('');
+	const [albumYear, setAlbumYear] = useState('');
+	const [albumGenre, setAlbumGenre] = useState('');
+	const [albumImage, setAlbumImage] = useState('');
+
+	const [featuresVisible, setFeaturesVisible] = useState(false);
+
+	const addAlbum = (event) => {
+		event.preventDefault();
+
+		props.setAlbums([
+			...props.albums,
+			{
+				id: albumId,
+				title: albumTitle,
+				artist: albumArtist,
+				releaseYear: albumYear,
+				genre: albumGenre,
+				imgUrl: albumImage,
+			},
+		]);
+
+		// Increment our id after each addition
+		setAlbumId((prevAlbumId) => prevAlbumId + 1);
+
+		// Reset input fields after form submission
+		setAlbumTitle('');
+		setAlbumArtist('');
+		setAlbumYear('');
+		setAlbumGenre('');
+		setAlbumImage('');
+	};
+	return (
+		<>
+			{!featuresVisible && (
+				<button
+					onClick={() =>
+						setFeaturesVisible(
+							(prevFeaturesVisible) => !prevFeaturesVisible
+						)
+					}
+				>
+					+ Add album
+				</button>
+			)}
+			{featuresVisible && (
+				<div className="add-album-container">
+					<form action="#" className="add-album-form" onSubmit={(event) => addAlbum(event)}>
+						<input
+							type="text"
+							className="album-input"
+							placeholder="Enter the album title..."
+							value={albumTitle}
+							onChange={(event) => setAlbumTitle(event.target.value)}
+							required
+						/>
+						<input
+							type="text"
+							className="album-input"
+							placeholder="Add the artist..."
+							value={albumArtist}
+							onChange={(event) => setAlbumArtist(event.target.value)}
+							required
+						/>
+						<input
+							type="number"
+							min="1500"
+							max="2099"
+							step="1"
+							className="album-input"
+							placeholder="Year released?"
+							value={albumYear}
+							onChange={(event) => setAlbumYear(event.target.value)}
+							required
+						/>
+						<input
+							type="text"
+							className="album-input"
+							placeholder="Genre?"
+							value={albumGenre}
+							onChange={(event) => setAlbumGenre(event.target.value)}
+							required
+						/>
+						<input
+							type="img"
+							className="album-input"
+							placeholder="(optional) Image URL for your album"
+							value={albumImage}
+							onChange={(event) => setAlbumImage(event.target.value)}
+						/>
+						<button type="submit">
+							+ Add album
+						</button>
+					</form>
+				</div>
+			)}
+		</>
+	);
+};
+
+export default AlbumForm;
