a7c2d31c412564ef7e1c0ff9f8f0ce4e60a29bd0
benjaminlangenaken <benjamin.langenaken@outlook.com>
2022-04-21 14:08:46 +0200

    Add URL validation for images

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const AlbumList = (props) => {
+	const urlPatternValidation = (URL) => {
+		const regex = new RegExp('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?');
+		return regex.test(URL);
+	};
+
 	return (
 		<>
 			<div className="album-list-container">
@@ -11,10 +16,19 @@ const AlbumList = (props) => {
 							<h3>{album.genre}</h3>
 						</div>
 						<div className="title-artist">
-						<h2>{album.title}</h2>
-						<h2>{album.artist}</h2>
+							<h2>{album.title}</h2>
+							<h2>{album.artist}</h2>
 						</div>
-						<img src={album.imgUrl} alt="album cover" />
+						{!urlPatternValidation(album.imgUrl)
+							? (
+						<img
+							src={require("../images/vinyl.png")}
+							className="placeholder-img"
+							alt="album cover"
+						/>):(<img
+							src={album.imgUrl}
+							alt="album cover"
+						/>)}
 					</div>
 				))}
 			</div>
